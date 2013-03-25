528d5b9b436990942bb3fecf3c19232545709770
Javier Cabezas <javier.cabezas@gmail.com>
2013-03-25 16:51:03 +0100

    Add setup.py installation file

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='cudaprof',
+      version='0.1',
+      description='CUDA Profiler Tools',
+      author='Javier Cabezas',
+      author_email='javier.cabezas@gmail.com',
+      url='https://code.google.com/p/cuda-profiler-tools/',
+      packages=[ 'cudaprof', 'cudaprof.gui' ],
+      package_dir={'cudaprof': 'tools/cudaprof'},
+      scripts=['tools/cuda-profiler.py']
+)
+
+# vim:set backspace=2 tabstop=4 shiftwidth=4 textwidth=120 foldmethod=marker expandtab:
