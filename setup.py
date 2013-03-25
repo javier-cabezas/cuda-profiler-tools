e6dd285f9fb9dd00246c8619ef1a913362bfe7e5
Javier Cabezas <javier.cabezas@gmail.com>
2013-03-25 17:57:15 +0100

    Renamed profiler tool

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='cudaprof',
       url='https://code.google.com/p/cuda-profiler-tools/',
       packages=[ 'cudaprof', 'cudaprof.gui' ],
       package_dir={'cudaprof': 'tools/cudaprof'},
-      scripts=['tools/cuda-profiler.py']
+      scripts=['tools/cuda-profiler']
 )
 
 # vim:set backspace=2 tabstop=4 shiftwidth=4 textwidth=120 foldmethod=marker expandtab:
